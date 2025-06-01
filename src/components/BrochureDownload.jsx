2ff8a187b073a018cda0d8134b33fc1060a7f3a5
Ashish <ashishkumarbhagat21@gmail.com>
2025-06-01 11:38:26 +0530

    broucher

--- src/components/BrochureDownload.jsx
+++ src/components/BrochureDownload.jsx
@@ -8,7 +8,7 @@ function BrochureDownload() {
         <div className="brochure-content">
           <h2>Halcyon 2025 Brochure</h2>
           <p>Download our official event brochure to learn more about all the exciting events and activities.</p>
-          <a href="/assets/Halcyon 2025 Brochure.pdf" download className="download-button">
+          <a href="/assets/Halcyon_2025.pdf" download className="download-button">
             <i className="fas fa-download"></i> Download Brochure
           </a>
         </div>
